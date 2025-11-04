7e4f93d426e7b9f8b099ee54819c2433260763d3
Nigel James <nigel.james@squarecloud.com.au>
2025-11-04 23:32:59 +1100

    finished gigasecond

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,6 +1,8 @@
 use time::PrimitiveDateTime as DateTime;
+use time::Duration;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    todo!("What time is a gigasecond later than {start}");
+    // let gigasec:u32 = 1_000_000_000;
+    start + Duration::seconds(1_000_000_000)
 }
