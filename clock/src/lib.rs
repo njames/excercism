3810880012902e4062eba7e93a26eea9a2fbb499
Nigel James <nigel.james@squarecloud.com.au>
2025-11-06 16:05:58 +1100

    corrections to clock

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,11 +1,36 @@
-pub struct Clock;
+use std::fmt;
+
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        todo!("Construct a new Clock from {hours} hours and {minutes} minutes");
+        let base_clock = Self {
+            hours: 0,
+            minutes: 0,
+        };
+        let minutes_to_add = (hours * 60) + minutes;
+        base_clock.add_minutes(minutes_to_add)
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        todo!("Add {minutes} minutes to existing Clock time");
+        let total_minutes = (self.hours * 60) + self.minutes + minutes;
+
+        let new_minutes = total_minutes.rem_euclid(60);
+        let new_hours = (total_minutes.div_euclid(60)).rem_euclid(24);
+
+        Self {
+            hours: new_hours,
+            minutes: new_minutes,
+        }
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
